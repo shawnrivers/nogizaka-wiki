04aa8c8cb044b01c4c3d2831c93d7f8e00c92d31
Shawn Rivers <kaushou@gmail.com>
2019-04-12 21:45:52 +0900

    Add a test

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import * as line from '@line/bot-sdk';
+
+const LINE_CONFIG = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.LINE_CHANNEL_SECRET || '',
+};
+
+const lineClient = new line.Client(LINE_CONFIG);
+
+const server = express();
+
+server.post('/webhook', line.middleware(LINE_CONFIG), (request, response) => {
+  response.sendStatus(200);
+
+  for (const event of request.body.events) {
+    console.log('event:', event);
+    lineClient.replyMessage(event.replyToken, {
+      type: 'text',
+      text: 'Hello, world',
+    });
+  }
+});
+
+server.listen(process.env.PORT || 8080);
