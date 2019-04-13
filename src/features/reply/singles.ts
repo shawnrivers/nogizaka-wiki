90ef04c130b18b03d304b7649dd3efae27d60a76
Shawn Rivers <kaushou@gmail.com>
2019-04-13 12:47:43 +0900

    Add function to return single Flex Message reply object

--- src/features/reply/singles.ts
+++ src/features/reply/singles.ts
@@ -1,6 +1,7 @@
-import { TextMessage } from '@line/bot-sdk';
+import { TextMessage, FlexMessage, FlexContainer } from '@line/bot-sdk';
 import { ISingle } from '../../models/ISingle';
-import { getSingleTitle } from '../search/singles';
+import { getSingleTitle, getSingleByNumber } from '../search/singles';
+import { getSingleFlexMessage } from '../messages/flexMessage/single';
 
 export const replyWithSingleTitle = (query: string, singles: ISingle[]): TextMessage => {
   const queryInt = parseInt(query);
@@ -13,6 +14,32 @@ export const replyWithSingleTitle = (query: string, singles: ISingle[]): TextMes
       type: 'text',
       text: replyText,
     };
+  } else {
+    return {
+      type: 'text',
+      text: 'Please type in an integer between 1 to 22.',
+    };
+  }
+};
+
+export const replyWithSingleFlexMessage = (query: string, singles: ISingle[]): FlexMessage | TextMessage => {
+  const queryInt = parseInt(query);
+
+  if (queryInt && queryInt > 0 && queryInt < 23) {
+    const single = getSingleByNumber(query, singles);
+
+    if (single) {
+      return {
+        type: 'flex',
+        altText: single.title,
+        contents: getSingleFlexMessage(single) as FlexContainer,
+      };
+    } else {
+      return {
+        type: 'text',
+        text: "Sorry, didn't match.",
+      };
+    }
   } else {
     return {
       type: 'text',
