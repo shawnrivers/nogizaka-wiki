d8fcc5d03902cf65660c075aae1fba6eb9cc1749
Shawn Rivers <kaushou@gmail.com>
2019-04-13 12:46:27 +0900

    Add Flex Message Hero template

---
+++ src/features/messages/flexMessage/templates/hero.ts
@@ -0,0 +1,14 @@
+// prettier-ignore
+export const getHero = () => {
+  return {
+    "type": "image",
+    "url": "https://raw.githubusercontent.com/shawnrivers/nogizaka-data/master/src/images/artworks/singles/22/A_medium.jpg",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    }
+  };
+}
