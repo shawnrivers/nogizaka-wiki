1627a8cf5cbcc4e74113e3cfb08d3f4a1c58763c
Shawn Rivers <kaushou@gmail.com>
2019-04-13 12:48:23 +0900

    Add searcher to get ISingle object

--- src/features/search/singles.ts
+++ src/features/search/singles.ts
@@ -1,5 +1,21 @@
 import { ISingle } from '../../models/ISingle';
 
+export const getSingleByNumber = (singleNumber: string, singles: ISingle[]): ISingle | undefined => {
+  for (const single of singles) {
+    if (singleNumber === single.number) {
+      return single;
+    }
+  }
+};
+
+export const getSingleByTitle = (singleTitle: string, singles: ISingle[]): ISingle | undefined => {
+  for (const single of singles) {
+    if (single.title.includes(singleTitle)) {
+      return single;
+    }
+  }
+};
+
 export const getSingleTitle = (singleNumber: string, singles: ISingle[]): string => {
   for (const single of singles) {
     if (singleNumber === single.number) {
