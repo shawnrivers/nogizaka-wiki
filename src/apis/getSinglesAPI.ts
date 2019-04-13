0b0843154182127dadd7126100c9b9d0cd1c4776
Shawn Rivers <kaushou@gmail.com>
2019-04-13 22:45:44 +0900

    Convert song types in singles API response

--- src/apis/getSinglesAPI.ts
+++ src/apis/getSinglesAPI.ts
@@ -1,6 +1,45 @@
-import { SingleResponse } from './types/SinglesResponseTypes';
+import { SingleResponse, CdSong } from './types/SinglesResponseTypes';
 import { ISingle } from '../models/ISingle';
 import * as request from 'request-promise';
+import { APIResponseSongType, SongType } from '../utils/constants';
+import { ICdSong } from '../models/ICd';
+
+const convertSongType = (songType: APIResponseSongType): SongType => {
+  switch (songType) {
+    case APIResponseSongType.None:
+      return SongType.None;
+    case APIResponseSongType.Title:
+      return SongType.Title;
+    case APIResponseSongType.Coupling:
+      return SongType.Coupling;
+    case APIResponseSongType.Under:
+      return SongType.Under;
+    case APIResponseSongType.Unit:
+      return SongType.Unit;
+    case APIResponseSongType.Solo:
+      return SongType.Solo;
+    case APIResponseSongType.FirstGeneration:
+      return SongType.FirstGeneration;
+    case APIResponseSongType.SecondGeneration:
+      return SongType.SecondGeneration;
+    case APIResponseSongType.ThirdGeneration:
+      return SongType.ThirdGeneration;
+    case APIResponseSongType.FourthGeneration:
+      return SongType.FourthGeneration;
+  }
+};
+
+const convertSongs = (songs: CdSong[]): ICdSong[] => {
+  let convertedSongs: ICdSong[] = [];
+  for (const song of songs) {
+    convertedSongs.push({
+      ...song,
+      type: convertSongType(song.type),
+    });
+  }
+
+  return convertedSongs;
+};
 
 const convertSingleResponse = (singleResponse: SingleResponse): ISingle => {
   const { number, title, release, artworks, shopping, songs, behindPerformers } = singleResponse;
@@ -11,7 +50,7 @@ const convertSingleResponse = (singleResponse: SingleResponse): ISingle => {
     release,
     artworks,
     shopping,
-    songs,
+    songs: convertSongs(songs),
     behindPerformers,
   };
 };
