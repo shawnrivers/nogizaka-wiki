e8fd1405a38a471faf1fa184318ee7c9742854c8
Shawn Rivers <kaushou@gmail.com>
2019-04-13 10:56:17 +0900

    Add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,44 @@
+export enum CdsCurrentPage {
+  Single = 'singles',
+  Album = 'albums',
+}
+
+export enum CdType {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  T = 'T',
+  L = 'Limited',
+}
+
+export enum SongType {
+  None = 'none',
+  Title = 'title',
+  Coupling = 'coupling',
+  Under = 'under',
+  Unit = 'unit',
+  Solo = 'solo',
+  FirstGeneration = 'first generation',
+  SecondGeneration = 'second generation',
+  ThirdGeneration = 'third generation',
+}
+
+export enum FukujinType {
+  RowOne = "row-1",
+  RowOneTwo = "row-1-2",
+}
+
+export enum FocusPerformersType {
+  Center = 'center',
+  Solo = 'solo',
+  Unit = 'unit',
+  None = '',
+}
+
+export enum FetchStatus {
+  None,
+  Rejected,
+  Pending,
+  Fulfilled,
+}
