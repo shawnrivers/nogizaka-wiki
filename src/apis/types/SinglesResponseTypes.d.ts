f226836ecc4fd9c64d11813da9f14630dc048076
Shawn Rivers <kaushou@gmail.com>
2019-04-13 10:57:53 +0900

    Declare API response types

---
+++ src/apis/types/SinglesResponseTypes.d.ts
@@ -0,0 +1,47 @@
+import { CdType, FocusPerformersType, SongType } from '../../utils/constants';
+
+type CdArtwork = {
+  type: CdType;
+  urls: {
+    large: string;
+    medium: string;
+    small: string;
+  };
+};
+
+type FocusPerformers = {
+  type: FocusPerformersType;
+  name: string[];
+};
+
+type Site = {
+  title: string;
+  url: string;
+};
+
+type CdSong = {
+  number: number;
+  title: string;
+  inCdType: CdType[];
+  type: SongType;
+  artwork: string;
+  focusPerformers: FocusPerformers;
+};
+
+type CdResponse = {
+  title: string;
+  number: string;
+  release: string;
+  artworks: CdArtwork[];
+  shopping: Site[];
+  songs: CdSong[];
+};
+
+type SingleBehindPerformers = {
+  trainees: string[];
+  skips: string[];
+};
+
+export type SingleResponse = CdResponse & {
+  behindPerformers: SingleBehindPerformers;
+};
---
+++ src/apis/types/SongsResponseTypes.d.ts
@@ -0,0 +1,34 @@
+import { FukujinType, SongType } from '../../utils/constants';
+
+type SongCreators = {
+  lyrics: string[];
+  compose: string[];
+  arrange: string[];
+  direct: string[];
+};
+
+export type SongPerformers = {
+  center: string[];
+  fukujin: FukujinType | string[];
+  solo: string;
+  unit: string;
+};
+
+export type SongFormations = {
+  firstRow: string[];
+  secondRow: string[];
+  thirdRow: string[];
+  fourthRow: string[];
+};
+
+export type SongResponse = {
+  title: string;
+  single: string;
+  artwork: string;
+  musicVideo: string;
+  albums: string[];
+  type: SongType;
+  creators: SongCreators;
+  performers: SongPerformers;
+  formations: SongFormations;
+};
